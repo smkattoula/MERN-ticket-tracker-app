207c6062702751fabfb10ddd25a723123c38559e
Shaker Kattoula <smkattoula@gmail.com>
2020-07-01 16:51:07 -0700

    add app.set

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+
+app.set("port", PORT);
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +26,6 @@ const usersRouter = require('./routes/users');
 app.use('/tickets', ticketsRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
