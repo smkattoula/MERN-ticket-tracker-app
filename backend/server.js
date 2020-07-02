0d2ee8ff8213969f51f14fa75ab656743bca3f31
Shaker Kattoula <smkattoula@gmail.com>
2020-07-02 13:09:55 -0700

    add mongolab and MONGODB_URI

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI || process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
