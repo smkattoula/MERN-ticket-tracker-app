3d21db405d90d63f0d7d275683858e9fae92785e
Shaker Kattoula <smkattoula@gmail.com>
2020-07-01 21:18:18 -0700

    server.js configuration

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000
-
-app.set("port", PORT);
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +24,14 @@ const usersRouter = require('./routes/users');
 
 app.use('/tickets', ticketsRouter);
 app.use('/users', usersRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+} 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
---
+++ build/precache-manifest.ef9ff87c124f1a3afbfa6af10d8c96cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4133c8d8b8aec22927fa0961a31999d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0242a04fea7f62679b87",
+    "url": "/static/css/2.1601c95f.chunk.css"
+  },
+  {
+    "revision": "fcc6ecc73becc0c2c74d",
+    "url": "/static/css/main.7577b1d2.chunk.css"
+  },
+  {
+    "revision": "0242a04fea7f62679b87",
+    "url": "/static/js/2.d22d8d27.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.d22d8d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc6ecc73becc0c2c74d",
+    "url": "/static/js/main.1653e96c.chunk.js"
+  },
+  {
+    "revision": "f2331fcce232c91197f1",
+    "url": "/static/js/runtime-main.aac491ef.js"
+  }
+]);
