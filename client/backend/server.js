d0541c07e1ee153f8c6e0ac1d10fda98e10a057e
Shaker Kattoula <smkattoula@gmail.com>
2020-07-03 11:00:18 -0700

    server.js config and package.json config

--- client/backend/server.js
+++ client/backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || "http://localhost:5000/";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
