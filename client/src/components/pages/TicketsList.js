1ea38f8e453ddd256bfd19fd0d291913feecc605
Shaker Kattoula <smkattoula@gmail.com>
2020-07-03 11:12:37 -0700

    Revert "change HTTP request url in TicketsList.js"
    This reverts commit 4377efa2a6eea9ec8bda3d95e754fd29651e9943.

--- client/src/components/pages/TicketsList.js
+++ client/src/components/pages/TicketsList.js
@@ -25,7 +25,7 @@ export default class TicketsList extends Component {
     }
 
     componentDidMount() {
-        axios.get('/tickets/')
+        axios.get('http://localhost:5000/tickets/')
             .then(response => {
                 this.setState({ tickets: response.data })
             })
@@ -35,7 +35,7 @@ export default class TicketsList extends Component {
     }
 
     deleteTicket(id) {
-        axios.delete('/tickets/'+id)
+        axios.delete('http://localhost:5000/tickets/'+id)
             .then(res => console.log(res.data));
         this.setState({
             tickets: this.state.tickets.filter(el => el._id !== id)
